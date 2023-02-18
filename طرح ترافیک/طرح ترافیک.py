dcaad1fa66a5a3bd7fd75cbd0eed99854184e607
AmirAshkanE <masterium92@gmail.com>
2023-02-18 21:56:15 +0330

    best i could get

--- طرح ترافیک/طرح ترافیک.py
+++ طرح ترافیک/طرح ترافیک.py
@@ -9,9 +9,7 @@ class User:
         self.user_name = name
         self.car_list = []
         self.penalty_amount = 0
-        self.balance_amount = 0
-        self.license_day_count = 1
-
+        self.balance_amount = 0       
 
 def day_counter(date):
     weekdays = ["Sat", "Sun", "Mon", "Tues", "Wed", "Thurs", "Fri"]
@@ -45,15 +43,73 @@ def car_number_checker(car_number):
     else:
         return "is_odd"
 
+def license_expiry_calc(days,date):
+    _date = []
+    for d in date.split("/"):
+        _date.append(int(d))
+    _year,_month,_day = _date[0],_date[1],_date[2]
+    _day += (days)
+
+    if _day <= 30:
+        expiry_date = f"{_year}/{_month:02}/{_day:02}"
+        return expiry_date
+    else:
+        while _day > 30:
+                _day -= 30
+                _month += 1
+                if _month > 12:
+                    _month -= 12
+                    _year += 1
+        expiry_date = f"{_year}/{_month:02}/{_day:02}"
+        return expiry_date
+
+
+def expiry_checker(date1,date2):
+    _date1=[]
+    _date2=[]
+    for d in date1.split("/"):
+        _date1.append(int(d))
+    _year1,_month1,_day1 = _date1[0],_date1[1],_date1[2]
+    for d1 in date2.split("/"):
+        _date2.append(int(d1))
+    _year2,_month2,_day2 = _date2[0],_date2[1],_date2[2]
+    
+    if _year1-_year2 > 0:
+        return True
+    elif _month1 - _month2 > 0:
+        return True
+    elif _day1 - _day2 > 0:
+        return True
+    else:
+        return False
+    
+    
+
+def date_operator(date,num):
+    _date = []
+    for d in date.split("/"):
+        _date.append(int(d))
+    _year,_month,_day = _date[0],_date[1],_date[2]
+    _day += num
+    if _day > 30:
+        _day -= 30
+        _month +=1
+    if _month > 12:
+        _month -= 12
+        _year += 1
+    show_date = f"{_year}/{_month:02}/{_day:02}"
+    return show_date
 
 user_list = []
+username_list = []
 car_number_list = []
 car_dict = {}
+expiry_dir = {}
 
 while is_running:
     command, *data = input().split()
-
-    if command == "REGISTER":
+        
+    if command == "REGISTER":   
         for user in user_list:
             if data[0] == user.user_name:
                 print("INVALID USERNAME")
@@ -61,22 +117,26 @@ while is_running:
         else:
             print("REGISTER DONE")
             user_list.append(User(data[0]))
-
+            for user in user_list:
+                    if user.user_name not in username_list:
+                        username_list.append(user.user_name)
+    
     elif command == "REGISTER_CAR":
         for user in user_list:
             car_dict.update({user.user_name: user.car_list})
-
-        if data[0] not in car_dict.keys():
+            
+        if data[0] not in username_list:
             print("INVALID USERNAME")
-        elif data[1] in car_number_list:
+        elif data[1] in car_number_list or len(data[1]) != 10:
             print("INVALID CAR PLATE")
         else:
             for user in user_list:
-                if data[0] == user.user_name:
+                if user.user_name == data[0]:
                     user.car_list.append(data[1])
                     car_number_list.append(data[1])
                     print("REGISTER CAR DONE")
                     break
+                
 
     elif command == "NEW_RECORD":
         if data[0] not in car_number_list:
@@ -84,33 +144,54 @@ while is_running:
         else:
             day = day_checker(day_counter(data[1]))
             number = car_number_checker(data[0])
+            if expiry_dir != {}:
+                has_license = expiry_checker( expiry_dir.get(data[0]),data[1])
+            else:
+                has_license = False
             
             
-            if (number == "is_even" and day == "even_day") or day == "friday":
+            if day == "friday":
                 print("NORMAL RECORDED")
-            elif (number == "is_odd" and day == "odd_day") or day == "friday":
+            elif (number == "is_even" and day == "even_day" ):
+                print("NORMAL RECORDED")
+            elif (number == "is_odd" and day == "odd_day"):
+                print("NORMAL RECORDED")
+            elif has_license:
                 print("NORMAL RECORDED")
             else:
                 for user in user_list:
                     if data[0] in user.car_list:
                         print("PENALTY RECORDED")
                         user.penalty_amount += 100
+                        
+
     elif command == "BUY_LICENSE":
-        for user in user_list:
-            if data[0] == user.user_name:
+        
+        if data[0] not in username_list:
                 print("INVALID USERNAME")
-                break
-            elif data[1] not in car_number_list:
-                print("INVALID CAR PLATE")
-                break
-            elif user.user_name == data[0] and user.balance_amount < 70:
-                print("NO ENOUGH MONEY")
-                break
-            else:
-                print("BUY LICENSE DONE")
+        elif data[1] not in car_number_list:
+            print("INVALID CAR PLATE")
+        else:  
+            for user in user_list:
+                if user.user_name != data[0]:
+                    continue
+                elif data[1] not in user.car_list:
+                    print("INVALID CAR PLATE")
+                    break
+                elif user.balance_amount < 70:
+                    print("NO ENOUGH MONEY")
+                    break
+                elif user.balance_amount < int(data[2])*70:
+                        print("NO ENOUGH MONEY")
+                        break
+                else:
+                    user.balance_amount -= (int(data[2])*70)
+                    expiry_dir.update({data[1] : license_expiry_calc(int(data [2]), data[3])})
+                    print("BUY LICENSE DONE")
+                    break
                 
     elif command == "ADD_BALANCE":
-        if data[0] not in car_dict.keys():
+        if data[0] not in username_list:
             print("INVALID USERNAME")
         else:
             for user in user_list:
@@ -119,7 +200,7 @@ while is_running:
                     print("ADD BALANCE DONE")
                     break
     elif command == "GET_BALANCE":
-        if data[0] not in car_dict.keys():
+        if data[0] not in username_list:
             print("INVALID USERNAME")
         else:
             for user in user_list:
@@ -127,7 +208,7 @@ while is_running:
                     print(user.balance_amount)
                     break
     elif command == "GET_PENALTY":
-        if data[0] not in car_dict.keys():
+        if data[0] not in username_list:
             print("INVALID USERNAME")
         else:
             for user in user_list:
@@ -135,7 +216,18 @@ while is_running:
                     print(user.penalty_amount)
                     break
     elif command == "GET_LICENSE_DEADLINE":
-        pass
+        if data[0] not in car_number_list:
+            print("INVALID CAR PLATE")
+        else:
+            temp_date = expiry_dir.get(data[0]) if data[0] in expiry_dir.keys() else "1400/01/01"
+            has_expired = expiry_checker(data[1], temp_date)
+            if has_expired:
+                print(date_operator(data[1], 1))
+            else:
+                print(temp_date)
+            
 
     elif command.upper() == "END":
         is_running = False
+
+
